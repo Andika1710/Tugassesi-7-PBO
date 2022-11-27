1dd1eb3696aedea5afdd2caef2a2b29f3cf26827
Andhika Oktasandira <114379148+Andika1710@users.noreply.github.com>
2022-11-27 14:07:24 +0700

    Add files via upload

---
+++ Outer0.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Axioo
+ */
+public class Outer0 {
+     int number = 0;
+
+    void outerMethod() {
+       
+    class Inner {
+        public void print() {
+            System.out.println("Mengakses inner class yang ke: " + (number++));
+        }
+    }
+
+        Inner in = new Inner();
+        in.print();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Outer0 out = new Outer0();
+        out.outerMethod();
+        out.outerMethod();
+        out.outerMethod();
+        
+    }
+}
+
